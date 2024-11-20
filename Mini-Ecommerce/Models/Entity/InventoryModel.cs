07cd8a6aabb1b69d4d4fe077eba1d9b491ff14ba
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-11-20 19:55:57 -0300

    feat(InventoryModel): cria entidade de inventario

---
+++ Mini-Ecommerce/Models/Entity/InventoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Mini_Ecommerce.Models.Entity;
+
+public class InventoryModel
+{
+    public int InventoryId { get; set; }
+    public int ProductId { get; set; }
+    public int Stock { get; set; }
+    public int LowStockAlert { get; set; }
+    public DateTime LastUpdated { get; set; }
+    public ProductModel Product { get; set; }
+}
