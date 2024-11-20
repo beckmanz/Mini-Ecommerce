1dbe21598a44d9ddf555def3018965ce8fc7b475
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-11-20 18:19:47 -0300

    refactor(UserModel): atualiza propriedade da entidade
    - Atualiza a propriedade "Name" para "FullName"
    - Remove a propriédade "CreatedAt"

--- Mini-Ecommerce/Models/Entity/UserModel.cs
+++ Mini-Ecommerce/Models/Entity/UserModel.cs
@@ -7,12 +7,11 @@ namespace Mini_Ecommerce.Models.Entity;
 public class UserModel
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string FullName { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
     public Role Role { get; set; }
     public string Document { get; set; }
     public int PhoneNumber { get; set; }
     public DateOnly DateOfBirth { get; set; }
-    public DateOnly CreatedAt { get; set; }
 }
