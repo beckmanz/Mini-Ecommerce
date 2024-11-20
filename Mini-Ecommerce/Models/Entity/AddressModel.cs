cdc63508a9787eea430823275e615a0cd90edec5
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-11-20 18:25:15 -0300

    feat(AddressModel): cria entidade de endereço

---
+++ Mini-Ecommerce/Models/Entity/AddressModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Mini_Ecommerce.Models.Entity;
+
+public class AddressModel
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Street { get; set; }
+    public int NumberHouse { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public bool IsDefault { get; set; }
+}
