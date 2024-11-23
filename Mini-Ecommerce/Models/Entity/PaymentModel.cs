dd1e5f56178a2fe9884b7254cb8c9cfdc150cb4b
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-11-23 17:53:31 -0300

    feat(PaymentModel): implementa todas as propriedades da entidade

--- Mini-Ecommerce/Models/Entity/PaymentModel.cs
+++ Mini-Ecommerce/Models/Entity/PaymentModel.cs
@@ -2,5 +2,11 @@
 
 public class PaymentModel
 {
-    
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public decimal Amount { get; set; }
+    public string PaymentMethod { get; set; }
+    public string Status { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public OrderModel Order { get; set; }
 }
